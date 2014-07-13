8b45931f5b8ff3f112a1370b0e47b8d8a6b20025
Gezim Hoxha <hgezim@gmail.com>
2014-07-13 08:58:56 -0600

    Added builtin 'dj_debug'
    Added builtin that can be enabled or disabled via DEBUG_INSPECTOR_PANEL_EXTEND_BUILTINS setting

--- inspector_panel/__init__.py
+++ inspector_panel/__init__.py
@@ -1,3 +1,12 @@
-__all__ = ["panels"]
+from django.conf import settings
 
-from inspector_panel.panels.inspector import debug
+__all__ = ["panels"]
+if settings.DEBUG_INSPECTOR_PANEL_EXTEND_BUILTINS:
+    # ensure we're not overriding an existing builtin
+    try:
+        __builtins__['dj_debug']
+
+        raise NameError('dj_debug is already in use as a builtin. Set DEBUG_INSPECTOR_PANEL_EXTEND_BUILTINS setting to False.')
+    except AttributeError as e:
+        from inspector_panel.panels.inspector import debug
+        __builtins__['dj_debug'] = debug
