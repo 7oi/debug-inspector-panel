f7ea802864fb926c44de35eeda2619939ad315d9
Gezim Hoxha <hgezim@gmail.com>
2014-07-13 09:05:22 -0600

    Changed exception to handle to KeyError

--- inspector_panel/__init__.py
+++ inspector_panel/__init__.py
@@ -7,6 +7,6 @@ if settings.DEBUG_INSPECTOR_PANEL_EXTEND_BUILTINS:
         __builtins__['dj_debug']
 
         raise NameError('dj_debug is already in use as a builtin. Set DEBUG_INSPECTOR_PANEL_EXTEND_BUILTINS setting to False.')
-    except AttributeError as e:
+    except KeyError as e:
         from inspector_panel.panels.inspector import debug
         __builtins__['dj_debug'] = debug
