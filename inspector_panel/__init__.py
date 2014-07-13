35de063d90b7a4036952a3cb1e4c80d5e28dd1d6
Gezim Hoxha <hgezim@gmail.com>
2014-07-13 09:27:25 -0600

    Fixed shorthand way of importing debug funciton

--- inspector_panel/__init__.py
+++ inspector_panel/__init__.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 
 __all__ = ["panels"]
+from inspector_panel.panels.inspector import debug
+
 if settings.DEBUG_INSPECTOR_PANEL_EXTEND_BUILTINS:
     # ensure we're not overriding an existing builtin
     try:
@@ -8,5 +10,4 @@ if settings.DEBUG_INSPECTOR_PANEL_EXTEND_BUILTINS:
 
         raise NameError('dj_debug is already in use as a builtin. Set DEBUG_INSPECTOR_PANEL_EXTEND_BUILTINS setting to False.')
     except KeyError as e:
-        from inspector_panel.panels.inspector import debug
         __builtins__['dj_debug'] = debug
